218150d460aa3752ca68cb019db68f63c42c1661
Samim Mirhosseini <samim.mirhoseini@gmail.com>
2017-04-05 20:37:02 -0400

    Showing the "id" of the server

--- Swarm/app.js
+++ Swarm/app.js
@@ -3,11 +3,13 @@ var fs      = require('fs')
 var app = express()
 // REDIS
 //var client = redis.createClient(6379, '127.0.0.1', {})
+var id = Math.floor(Math.random()*1000)
 
 ///////////// WEB ROUTES
 app.get('/', function (req, res) {
 	res.writeHead(200, { 'content-type': 'text/html' });
 	res.write('<h1>Hello world</h1>')
+	res.write(`Serving from container: ${id}`);
 	res.end();
 })
 
